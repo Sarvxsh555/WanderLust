c48a8e14f1abf9557820591ae0350e7404eddab7
Sarvxsh555 <sarveshvaranarun@gmail.com>
2025-12-21 23:31:46 +0530

    User SignUp and Login

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const {listingSchema,reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./models/user.js");
 
 const session = require('express-session');
 const flash = require('connect-flash');
 
 //Express Routers
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
 
-app.set("view engine","ejs");``
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
@@ -62,19 +66,35 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
-     res.locals.error = req.flash("error");
+    res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/demouser",async(req,res)=>{
+    let fakeUser = new User({
+        email : "student@gmail.com",
+        username : "delta-bro"
+    });
+   let registerdUser = await User.register(fakeUser,"password");
+   res.send(registerdUser);
 });
 
 
 //Routes/Listing.js
-app.use("/listings",listings);
+app.use("/listings",listingRouter);
 
 //Routes/review.js
-app.use("/listings/:id/review",reviews);
-
+app.use("/listings/:id/review",reviewRouter);
+app.use("/",userRouter);
 
 
 // app.all("/*",(req,res,next)=>{
